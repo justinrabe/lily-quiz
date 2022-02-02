4ae64e1437b04ff5bce0a29f6a89855ba58fe4dd
justin <jtr342@gmail.com>
2022-02-01 22:53:41 -0800

    resolved issue with unordered answer/isCorrect pairs

--- dbconfig_test.py
+++ dbconfig_test.py
@@ -8,19 +8,23 @@ def query_with_fetchone():
         dbconfig = read_db_config()
         conn = MySQLConnection(**dbconfig)
         cursor = conn.cursor()
-        cursor.execute("SELECT question, name FROM questions q join answers a on q.id = a.questionID")
+        cursor.execute("SELECT question, name, isCorrect FROM questions q join answers a on q.id = a.questionID")
 
         row = cursor.fetchone()
 
+        ##for each entry in questions, there is a list of answers. 
+        ## TODO: add the 'correct bit into the lists.
         while row is not None:
             if row[0] in qaTotal.keys():
                 #question exists
-                qaTotal[row[0]].append(row[1])
+                qaTotal[row[0]].append({row[1]:row[2]})
             else:
-                #new question, insert new dictionary entry with answer as list of size 1
-                qaTotal[row[0]] = [row[1]]
+                #new question, insert new dictionary entry with answer as list of size 
+                qaTotal[row[0]] = [{row[1]: row[2]}]
+                print(qaTotal[row[0]])
             row = cursor.fetchone()
         print(qaTotal)
+       
 
     except Error as e:
         print(e)
