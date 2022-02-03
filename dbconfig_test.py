19ea113a65d95533154614e7280024056984d2f1
justin <jtr342@gmail.com>
2022-02-02 20:42:35 -0800

    working demo completed

--- dbconfig_test.py
+++ dbconfig_test.py
@@ -2,6 +2,7 @@ from mysql.connector import MySQLConnection, Error
 from python_mysql_dbconfig import read_db_config
 
 qaTotal = {}
+score = 0
 def fetch_sql():
     try:
       
@@ -22,8 +23,6 @@ def fetch_sql():
                 qaTotal[row[0]] = [{row[1]: row[2]}]
             row = cursor.fetchone()
 
-       
-
     except Error as e:
         print(e)
 
@@ -32,15 +31,32 @@ def fetch_sql():
         conn.close()
 
 
-if __name__ == '__main__':
-    fetch_sql()
-    print(qaTotal)
+def game_loop():
     print('Hi Lily!')
     print('This is my Valentines present to you hehe')
     print('It''s a small little question app I built of a few questions from our relationship')
     input("Press Enter to start the quiz!!")
+    correct = ''
     for question in qaTotal:
         print(question)
-        print(qaTotal[question])
+        for answer in qaTotal[question]:
+            for l in answer:
+                print(l) 
+                if answer[l] == 1:
+                    correct = l
+        ##print(*qaTotal[question], sep = "\n")
+        guess = input('Enter your answer: ')
+        if guess == correct:
+            print ("Correct!!")
+            score = score + 1
+            input('Press Enter for next question')
+        else:
+            print ("Wrong xD")
+            input('Press Enter for next question')
+    print("Score = ", score)
 
+if __name__ == '__main__':
+    fetch_sql()
+    game_loop()
+    
 
